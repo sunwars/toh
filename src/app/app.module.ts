8780b8611de37c01d3c8696d2ff9b874c5086a20
jiin <tlswldls7@gmail.com>
2018-10-27 12:26:01 +0900

    5강 라우팅
    라우팅테이블 설계하여 메뉴별 url을 만들어야 한다.
    해당패스에 컴포넌트는 부모컴포넌트의 router-outlet으로 들어간다.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,29 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import {FormsModule} from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HomeComponent } from './home/home.component';
+import { TodoComponent } from './todo/todo.component';
+import {Route, RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'todo', component: TodoComponent},
+  {path: 'heroes', component: HeroesComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HomeComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule, // ngModel 디렉티브를 갖고있다.
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
