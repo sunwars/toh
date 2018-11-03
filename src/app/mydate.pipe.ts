5c7b32ebd84703eea04f941f29ce030361721b54
jiin <tlswldls7@gmail.com>
2018-11-03 15:37:09 +0900

    파이프 - 분까지 자르는 파이프 구현

--- src/app/mydate.pipe.ts
+++ src/app/mydate.pipe.ts
@@ -7,11 +7,13 @@ export class MydatePipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
     console.log('first:', value, 'second: ', args);
-    if (args === '$') {
+    /* if (args === '$') {
       return (value as number) / 1100;
     } else if (args === 'yen') {
       return (value as number) / 120;
-    }
+    } */
+
+    return value.toString().substring(0, 16);
   }
 
 }
