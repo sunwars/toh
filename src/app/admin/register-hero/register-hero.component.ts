1654c1d6e4299e16533433155903e279fff004fb
jiin <tlswldls7@gmail.com>
2018-11-03 18:22:35 +0900

    관리자 모듈 - 리액티브 스타일폼
    유효성 추가

--- src/app/admin/register-hero/register-hero.component.ts
+++ src/app/admin/register-hero/register-hero.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register-hero',
@@ -11,8 +11,9 @@ export class RegisterHeroComponent implements OnInit {
 
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
-      name: 'aaa',
-      email: null,
+      name: [null, Validators.compose([Validators.required, Validators.minLength(3),
+      Validators.maxLength(20)])],
+      email: [null, Validators.compose([Validators.required, Validators.email])],
       sex: null,
       country: null,
       address: null
