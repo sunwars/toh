aed9e8fa8239ca70a6cdfc5ca5a9e88237f979ce
jiin <tlswldls7@gmail.com>
2018-11-03 17:41:32 +0900

    관리자 모듈 - 좌측 메뉴 구성

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   {path: '', component: IndexComponent, children: [
       {path: '', component: DashboardComponent},
       {path: 'register', component: RegisterHeroComponent},
-      {paht: 'manage', component: ManageHeroComponent}
+      {path: 'manage', component: ManageHeroComponent}
     ]}
 ];
 
